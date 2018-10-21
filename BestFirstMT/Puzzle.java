025cfe5759918d63ff55a5829069896c6bf6f014
Kristen <kkdvillegas@gmail.com>
2018-10-21 16:23:37 +0800

    test cases updated

--- BestFirstMT/Puzzle.java
+++ BestFirstMT/Puzzle.java
@@ -165,11 +165,11 @@ class MisplacedTiles implements H {
 
 public class Puzzle {
 
-    final static int[] GOAL = new int[] {0,1,2,3,4,5,6,7,8};
+    final static int[] GOAL = new int[] {1,2,3,4,5,6,7,8,0};
     final static HashSet <String> seen = new HashSet <String>();
 
     public static void main(String[] args) {
-        int[] init = new int[] {2,7,0,3,5,8,1,6,4};
+        int[] init = new int[] {7, 3, 4, 0, 1, 5, 2, 8, 6};
 
         State initialState = new State(init, null, 0, 0, computeH(init, GOAL));
         search(initialState, new MisplacedTiles());
